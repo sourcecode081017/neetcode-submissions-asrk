57525d7db783f70ef33bdf13bfcf3ddcb381e313
Anirudh Sivaramakrishnan <s.anirudh.93@gmail.com>
2026-05-30 14:27:39 -0500

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.go
@@ -0,0 +1,37 @@
+
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return (*h)[0]
+}
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *IntHeap) Push(i any) {
+	*h = append(*h, i.(int))
+}
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n - 1]
+	return x
+}
+
+
+
+
